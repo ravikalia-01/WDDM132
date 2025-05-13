dabd5ba80ac4119ecc6f5f26d5e4ed50e36d08c0
Ravi Kalia <kravi014@gmail.com>
2025-05-13 18:52:33 -0400

    assignment 1 done

--- src/first.jsx
+++ src/first.jsx
@@ -4,9 +4,12 @@ function First() {
   return (
     <div class="mainFirst">
       <h1 class="headFirst">WelCome</h1>
-      <h2 class="headSecond">WDDM_132 Week_1 Class</h2>
-      <h2>My bio.</h2>
-      <p class="paraFirst">I am Ravi Kumar and this is WDDM_132, Week_1 assignment. in project_1, I want to create a app for which help students in creating their resume. </p>
+      <h2 class="head2">WDDM_132 Week_1 Class</h2>
+      <div class="bio">
+        <h2 class="head2">My bio.</h2>
+        <img src="https://cdn-icons-png.freepik.com/256/703/703985.png?ga=GA1.1.177419505.1747176599&semt=ais_hybrid" alt="profile picture of boy" class="pic1" />
+      </div>
+        <p class="para1">I am Ravi Kumar and this is WDDM_132, Week_1 assignment. in project_1, I want to create a app for which help students in creating their resume. </p>
     </div>
   );
 }

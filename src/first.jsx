d76bf4a71cc9d8d914b000a0d55155bbba3b51dd
Ravi Kalia <kravi014@gmail.com>
2025-05-13 18:15:41 -0400

    first commit

---
+++ src/first.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./first.css";
+function First() {
+  return (
+    <div class="mainFirst">
+      <h1 class="headFirst">WelCome</h1>
+      <h2 class="headSecond">WDDM_132 Week_1 Class</h2>
+      <h2>My bio.</h2>
+      <p class="paraFirst">I am Ravi Kumar and this is WDDM_132, Week_1 assignment. in project_1, I want to create a app for which help students in creating their resume. </p>
+    </div>
+  );
+}
+
+export default First;
